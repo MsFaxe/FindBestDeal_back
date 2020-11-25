b03e2d2687eb98acb00a7226f8981533e7e86660
MsFaxe <anty.wjj@gmail.com>
2020-11-25 18:53:04 +0100

    mappers update

--- src/main/java/com/kodilla/games/mapper/GogMapper.java
+++ src/main/java/com/kodilla/games/mapper/GogMapper.java
@@ -3,9 +3,9 @@ package com.kodilla.games.mapper;
 import com.kodilla.games.gog.domain.GogApp;
 import com.kodilla.games.gog.domain.GogPrice;
 import com.kodilla.games.gog.domain.GogProduct;
-import com.kodilla.games.gog.domain.online.GogAppDto;
-import com.kodilla.games.gog.domain.online.GogPriceDto;
-import com.kodilla.games.gog.domain.online.GogProductDto;
+import com.kodilla.games.gog.domain.dto.single_gogGame.GogAppDto;
+import com.kodilla.games.gog.domain.dto.single_gogGame.GogPriceDto;
+import com.kodilla.games.gog.domain.dto.single_gogGame.GogProductDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -25,7 +25,7 @@ public class GogMapper {
         );
     }
 
-    public List<GogProduct> maptoProductList(List<GogProductDto> gogProductDtos) {
+    public List<GogProduct> mapToProductList(List<GogProductDto> gogProductDtos) {
         return gogProductDtos.stream()
                 .map(this::mapToGogProduct)
                 .collect(Collectors.toList());
@@ -33,7 +33,7 @@ public class GogMapper {
 
     public GogApp mapToGogApp(GogAppDto gogAppDto) {
         return new GogApp(
-                maptoProductList(gogAppDto.getProducts())
+                mapToProductList(gogAppDto.getProducts())
         );
     }
 }
--- src/main/java/com/kodilla/games/mapper/SteamMapper.java
+++ src/main/java/com/kodilla/games/mapper/SteamMapper.java
@@ -1,22 +1,36 @@
-package com.kodilla.games.steam.mapper;
+package com.kodilla.games.mapper;
 
+import com.kodilla.games.steam.domain.SteamApp;
+import com.kodilla.games.steam.domain.SteamApplist;
+import com.kodilla.games.steam.domain.SteamRoot;
+import com.kodilla.games.steam.domain.dto.apps_list.SteamAppDto;
+import com.kodilla.games.steam.domain.dto.apps_list.SteamApplistDto;
+import com.kodilla.games.steam.domain.dto.apps_list.SteamRootDto;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class SteamMapper {
-//    public SteamApp mapToSteamApp(SteamAppDto steamAppDto) {
-//        return new SteamApp(
-//                steamAppDto.getSteamGameDto().getGameDetails().getSteamAppid(),
-//                steamAppDto.getSteamGameDto().getGameDetails().getName(),
-//                steamAppDto.getSteamGameDto().getGameDetails().getPriceOverview().getFinalFormatted()
-//        );
-//    }
+    public SteamRoot mapToSteamRoot(SteamRootDto steamRootDto) {
+        return new SteamRoot(mapToSteamApplist(steamRootDto.getApplist()));
 
-//    public SteamAppDto mapToSteamAppDto(SteamApp steamApp) {
-//        return new SteamAppDto(
-//                steamApp.getId(),
-//                steamApp.getName(),
-//                steamApp.getFinalPrice()
-//        );
-//    }
+    }
+
+    public SteamApplist mapToSteamApplist (SteamApplistDto steamApplistDto) {
+        return new SteamApplist(mapToApplist(steamApplistDto.getApps()));
+    }
+    public SteamApp mapToSteamApp(SteamAppDto steamAppDto) {
+        return new SteamApp(
+                steamAppDto.getAppid(),
+                steamAppDto.getName()
+        );
+    }
+
+    public List<SteamApp> mapToApplist(List<SteamAppDto> steamAppList) {
+        return steamAppList.stream()
+                .map(this::mapToSteamApp)
+                .collect(Collectors.toList());
+    }
 }
